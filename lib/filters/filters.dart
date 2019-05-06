ff1e50d201a4ce80d806c86e82f706163f2f00c3
OCN Yang <ocnyang@gmail.com>
2019-05-06 15:25:56 +0800

    filters page layout over;

--- lib/filters/filters.dart
+++ lib/filters/filters.dart
@@ -1,17 +1,127 @@
 import 'package:flutter/material.dart';
 
-class FiltersTabPage extends StatefulWidget{
+class FiltersTabPage extends StatefulWidget {
   @override
   FiltersTabPageState createState() {
     return FiltersTabPageState();
   }
-
 }
 
-class FiltersTabPageState extends State<FiltersTabPage>{
+const List<String> categoryItems = <String>['Deserts', 'Drinks', 'Snacks', 'Breakfast', 'Mains', 'Starters'];
+const List<String> dietItems = <String>['Vegetarian', 'Meatless', 'Baked', 'Sugar-free', 'Alcohol-free', 'Pan-free'];
+const List<String> cuisineItems = <String>[
+  'Chinese',
+  'Italian',
+  'Asian',
+  'European',
+  'American',
+  'Indian',
+  'Spanish',
+  'Portuguese'
+];
+
+class FiltersTabPageState extends State<FiltersTabPage> {
+  String _selectCategory = '';
+  String _selectDiet = '';
+  String _selectCuisine = '';
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('filters'),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Filters",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        actions: <Widget>[
+          FlatButton(
+              shape: CircleBorder(),
+              onPressed: () {},
+              child: Text(
+                "Apply",
+                style: TextStyle(color: Colors.grey),
+              ))
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          buildSectionItemView("Category", categoryItems),
+          buildSectionItemView("Diet", dietItems),
+          buildSectionItemView("Cuisine", cuisineItems),
+        ],
+      ),
+    );
   }
 
+  buildSectionItemView(String title, List<String> items) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 10),
+          child: Text(
+            title,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 2, right: 2),
+          child: Wrap(
+            children: items.map<Widget>((String name) {
+              return Padding(
+                padding: EdgeInsets.only(right: 10, left: 10),
+                child: ChoiceChip(
+                  label: Text(
+                    name,
+                    style: TextStyle(
+                        color: ((title == "Category"
+                                    ? _selectCategory
+                                    : (title == "Diet" ? _selectDiet : _selectCuisine))) ==
+                                name
+                            ? Colors.white
+                            : Colors.grey),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.grey, width: 0.5), borderRadius: BorderRadius.circular(20)),
+                  backgroundColor: Colors.transparent,
+                  selectedColor: Colors.brown,
+                  selected:
+                      ((title == "Category" ? _selectCategory : (title == "Diet" ? _selectDiet : _selectCuisine))) ==
+                          name,
+                  onSelected: (bool value) {
+                    setState(() {
+                      String newValue = value ? name : '';
+                      switch (title) {
+                        case "Category":
+                          {
+                            _selectCategory = newValue;
+                          }
+                          break;
+                        case "Diet":
+                          {
+                            _selectDiet = newValue;
+                            break;
+                          }
+                        default:
+                          {
+                            _selectCuisine = newValue;
+                            break;
+                          }
+                      }
+                    });
+                  },
+                ),
+              );
+            }).toList(),
+          ),
+        )
+      ],
+    );
+  }
 }
